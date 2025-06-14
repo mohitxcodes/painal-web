afb1d0ee4bd67d26b7a6c15cc1813c9c23ef6902
Mohit Singh <mohitsinghx12@gmail.com>
2025-06-14 22:50:52 +0530

    fix the case sensative builds

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
-import Header from './components/header/Header'
-import Footer from './components/footer/Footer'
+import Footer from './components/Footer/Footer'
 import { Outlet } from 'react-router-dom'
+import Header from './components/Header/Header'
 function App() {
 
   return (
