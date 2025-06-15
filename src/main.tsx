fa7f15e11cb78690b0afc692ba069ebf626fe2bd
Mohit Singh <mohitsinghx12@gmail.com>
2025-06-15 22:46:45 +0530

    added book details

--- src/main.tsx
+++ src/main.tsx
@@ -7,6 +7,7 @@ import GalleryPage from './pages/gallery/GalleryPage.tsx'
 import HomePage from './pages/home/HomePage.tsx'
 import AncestryPage from './pages/ancestry/FamilyMemberPage.tsx'
 import FamilyTree from './pages/family-tree/FamilyPage.tsx'
+import BookPage from './pages/book/BookPage.tsx'
 const router = createBrowserRouter([
   {
     path: '/',
@@ -27,11 +28,13 @@ const router = createBrowserRouter([
       {
         path: '/vanshavali',
         element: <FamilyTree />
+      },
+      {
+        path: '/our-book',
+        element: <BookPage />
       }
     ],
   },
-
-
 ])
 
 createRoot(document.getElementById('root')!).render(
